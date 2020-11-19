6780e3f899ea2bf88fdcd2c0a3bf6c832b27402c
Tomáš Batěk <batekto2@fit.cvut.cz>
2020-11-19 18:19:47 +0100

    Add inversion, custom character set, folder selection

--- src/Application.py
+++ src/Application.py
@@ -1,3 +1,5 @@
+from pyface.constant import OK
+from pyface.file_dialog import FileDialog
 from traits.api import *
 from traitsui.api import *
 
@@ -14,6 +16,22 @@ class Application(HasTraits):
     animator = Instance(Animator)
     editor = Instance(Editor)
 
+    select_file = Button('Select file with custom character set')
+
+    display_char_set = Str('<default character set>')
+
+    invert = Bool(False)
+
+    dialog = FileDialog()
+
+    def _select_file_fired(self):
+        result = self.dialog.open()
+        if result == OK:
+            if len(self.dialog.path) > 40:
+                self.display_char_set = '...' + self.dialog.path[-40:]
+            else:
+                self.display_char_set = self.dialog.path
+
     def _converter_default(self):
         return Converter()
 
@@ -24,11 +42,20 @@ class Application(HasTraits):
         return Editor()
 
     view = View(
-        Group(
-            Item("converter", show_label=False, style='custom', dock="tab"),
-            Item("animator", style='custom', show_label=False, dock="tab"),
-            Item("editor", style='custom', show_label=False, dock="tab"),
-            layout='tabbed'
+        VGroup(
+            VGroup(
+                HGroup(
+                    Item('select_file', show_label=False),
+                    Item('display_char_set', show_label=False, style='readonly')
+                ),
+                Item('invert', label='Invert characters (suitable for dark background)')
+            ),
+            Group(
+                Item('converter', show_label=False, style='custom', dock='tab'),
+                Item('animator', style='custom', show_label=False, dock='tab'),
+                Item('editor', style='custom', show_label=False, dock='tab'),
+                layout='tabbed'
+            )
         ),
         resizable=True,
         title='ASCII-art generator'
--- src/Converter.py
+++ src/Converter.py
@@ -1,31 +1,43 @@
+from pyface.constant import OK
+from pyface.directory_dialog import DirectoryDialog
 from traits.api import *
 from traitsui.api import *
 
-from src.FileDialog import FileDialog
-
 
 class Converter(HasTraits):
+    # Max width for the conversion:
     width = Int()
+
     convert = Button()
+
     load = Button()
-    load_custom_chars = Button("Load custom character set")
-    chars_dialog = Instance(Handler)
 
-    def _load_custom_chars_fired(self):
-        self.chars_dialog.edit_traits()
+    select_folder = Button('Select folder with pictures')
 
-    def _chars_dialog_default(self):
-        return FileDialog()
+    display_folder_path = Str('<folder not selected>')
+
+    # A platform-independent dialog to select a directory with images:
+    dialog = DirectoryDialog()
+
+    # If user clicks on 'Select folder with pictures':
+    def _select_folder_fired(self):
+        result = self.dialog.open()
+        if result == OK:
+            if len(self.dialog.path) > 40:
+                self.display_folder_path = '...' + self.dialog.path[-40:]
+            else:
+                self.display_folder_path = self.dialog.path
 
     view = View(
         VGroup(
             HGroup(
-                Item("load_custom_chars", show_label=False)
+                Item('select_folder', show_label=False),
+                Item('display_folder_path', style='readonly', show_label=False)
             ),
             HGroup(
-                Item("load", show_label=False),
-                Item("width"),
-                Item("convert", show_label=False)
+                Item('load', show_label=False),
+                Item('width'),
+                Item('convert', show_label=False)
             )
         )
     )
