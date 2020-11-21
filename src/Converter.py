0d541890942c1592ab72db036d628a302cb83108
Tomáš Batěk <batekto2@fit.cvut.cz>
2020-11-21 13:29:35 +0100

    Make threads for converter and loader
    Loading images and converting is now done in seperate threads.

--- src/Converter.py
+++ src/Converter.py
@@ -8,10 +8,16 @@ from pyface.directory_dialog import DirectoryDialog
 from traits.api import *
 from traitsui.api import *
 
+from src.ConverterThread import ConverterThread
+from src.LoaderThread import LoaderThread
 
-class ConverterHandler(Thread):
-    def run(self):
-        pass
+
+class InfoLabel(HasTraits):
+    label = Str()
+
+    view = View(
+        Item('label', show_label=False, style='readonly')
+    )
 
 
 class Converter(HasTraits):
@@ -52,104 +58,20 @@ class Converter(HasTraits):
         if self.selected_folder == '__none__':
             self.info_label = 'No folder has been selected.'
         else:
-            # Create '.tmp' directory:
-            if not os.path.exists('.tmp'):
-                os.mkdir('.tmp')
-
-            # Create string for commands:
-            to_write = ''
-            if self.invert:
-                to_write += 'invert true\n'
-            to_write += 'folder\n'
-            to_write += self.selected_folder[1:] + '\n'
-            to_write += 'converter\nload\n'
-
-            # Write commands file:
-            with open('.tmp/input', mode='w', encoding='utf-8') as file:
-                file.write(to_write)
-
             self.info_label = 'Loading...'
-
-            # Run generator with created input:
-            if not os.path.exists('generator'):
-                self.info_label = 'Generator executable not found. Have you built this project correctly?\n'
-            os.system('./generator <.tmp/input 1>.tmp/output 2>.tmp/error')
-
-            if not os.path.exists('.tmp/output'):
-                error('Output not created')
-
-            # Read the generator output and get the info from it:
-            output_str = ''
-            with open('.tmp/output', mode='r', encoding='utf-8') as file:
-                output_str = file.read()
-            lines = output_str.split('\n')
-            imgs_loaded = int(lines[-4][7])
-            output_str = ''
-            for line in lines[-4-imgs_loaded:-4+1]:
-                output_str += line + '\n'
-            self.info_label = output_str
-
-            # Delete temporary stuff:
-            os.remove('.tmp/input')
-            os.remove('.tmp/output')
-            os.remove('.tmp/error')
-            os.removedirs('.tmp')
+            loader = LoaderThread(self)
+            loader.start()
 
     def _convert_fired(self):
         if self.selected_folder == '__none__':
             self.info_label = 'No folder has been selected.'
         else:
             self.info_label = 'Converting...'
+            converter = ConverterThread(self)
+            converter.start()
 
-            # Create '.tmp' directory:
-            if not os.path.exists('.tmp'):
-                os.mkdir('.tmp')
 
-            # Create string for commands:
-            to_write = ''
-            if self.invert:
-                to_write += 'invert true\n'
-            to_write += 'folder\n'
-            to_write += self.selected_folder[1:] + '\n'
-            to_write += 'converter\nload\n'
-            to_write += 'width ' + str(self.width) + '\n'
-            to_write += 'convert\n'
 
-            # Write commands file:
-            with open('.tmp/input', mode='w', encoding='utf-8') as file:
-                file.write(to_write)
-
-            print('before')
-
-            # Run generator with created input:
-            if not os.path.exists('generator'):
-                self.info_label = 'Generator executable not found. Have you built this project correctly?\n'
-            os.system('./generator <.tmp/input 1>.tmp/output 2>.tmp/error')
-            print('after')
-
-            if not os.path.exists('.tmp/output'):
-                error('Output not created')
-
-            # Read the generator output and get the info from it:
-            output_str = ''
-            with open('.tmp/output', mode='r', encoding='utf-8') as file:
-                output_str = file.read()
-            lines = output_str.split('\n')
-            k = -4
-            line = lines[k]
-            output_str = ''
-            while 'Convert' in line:
-                if 'Converted' in line:
-                    output_str += line + '\n'
-                k -= 1
-                line = lines[k]
-            self.info_label = output_str
-
-            # Remove temporary stuff:
-            os.remove('.tmp/input')
-            os.remove('.tmp/output')
-            os.remove('.tmp/error')
-            os.removedirs('.tmp')
 
     view = View(
         VGroup(
---
+++ src/LoaderThread.py
@@ -0,0 +1,51 @@
+import os
+from threading import Thread
+
+
+class LoaderThread(Thread):
+    def __init__(self, converter):
+        super().__init__()
+        # Need reference to converter to be able to change GUI items:
+        self.converter = converter
+
+    def run(self):
+        # Create '.tmp' directory:
+        if not os.path.exists('.tmp'):
+            os.mkdir('.tmp')
+
+        # Create string for commands:
+        to_write = ''
+        if self.converter.invert:
+            to_write += 'invert true\n'
+        to_write += 'folder\n'
+        to_write += self.converter.selected_folder[1:] + '\n'
+        to_write += 'converter\nload\n'
+
+        # Write commands file:
+        with open('.tmp/input', mode='w', encoding='utf-8') as file:
+            file.write(to_write)
+
+        # Run generator with created input:
+        if not os.path.exists('generator'):
+            self.converterinfo_label = 'Generator executable not found. Have you built this project correctly?\n'
+        os.system('./generator <.tmp/input 1>.tmp/output 2>.tmp/error')
+
+        if not os.path.exists('.tmp/output'):
+            os.error('Output not created')
+
+        # Read the generator output and get the info from it:
+        output_str = ''
+        with open('.tmp/output', mode='r', encoding='utf-8') as file:
+            output_str = file.read()
+        lines = output_str.split('\n')
+        imgs_loaded = int(lines[-4][7])
+        output_str = ''
+        for line in lines[-4 - imgs_loaded:-4 + 1]:
+            output_str += line + '\n'
+        self.converter.info_label = output_str
+
+        # Delete temporary stuff:
+        os.remove('.tmp/input')
+        os.remove('.tmp/output')
+        os.remove('.tmp/error')
+        os.removedirs('.tmp')
