322dc5d0f89a5e4f9570aeabf612ff947dc732c2
Tomáš Batěk <batekto2@fit.cvut.cz>
2020-11-22 22:55:01 +0100

    Make test.py able to view ascii files.
    Note: before applying this method to the main program, there should be some form of a check
    for the size of the image, if it's too big, it tends to load for too long.

--- test.py
+++ test.py
@@ -1,34 +1,68 @@
 from traits.api import *
 from traitsui.api import *
 
-from test_othefile import Sub
+
+class Show(HasTraits):
+    def __init__(self, ascii, font_size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ascii = ascii
+        self.style_sheet = '*{font-size:' + str(font_size) + 'px; font-family:"Menlo"}'
 
 
-class Main(HasTraits):
-    sub = Instance(Sub)
+    ascii = Str()
 
-    invert = Bool()
 
-    button = Button('Change')
 
-    def _button_fired(self):
-        self.invert = not self.invert
 
-    def _sub_default(self):
-        return Sub(self)
+class MainWindow(HasTraits):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        str = ''
+        with open('/Users/tom/Desktop/converted/Eliška.png.ascii', mode='r+', encoding='utf-8') as f:
+            str = f.read()
+        self.ascii = str
+
+    ascii = Str()
+
+    font_size = Int(5)
+
+    smaller = Button()
+
+    def _smaller_fired(self):
+        self.font_size -= 1
+
+    bigger = Button()
+
+    def _bigger_fired(self):
+        self.font_size += 1
+
+    show = Button()
+
+    def _show_fired(self):
+        shower = Show(self.ascii, self.font_size)
+        style_sheet = '*{font-size:' + str(self.font_size) + 'px; font-family:"Menlo"}'
+
+        new_view = View(
+            Item('ascii', style='readonly', show_label=False, style_sheet=style_sheet)
+        )
+        shower.configure_traits(view=new_view)
 
     view = View(
         VGroup(
-            Item('invert', show_label=False, style='readonly'),
-            Item('button', show_label=False),
-            Item('sub', show_label=False, style='custom', dock='tab'),
-            layout='tabbed'
-        )
+            HGroup(
+                Item('smaller', show_label=False),
+                Item('font_size', show_label=False, style='readonly'),
+                Item('bigger', show_label=False)
+            ),
+            Item('show', show_label=False)
+        ),
+        resizable=True,
+        title='Test'
     )
 
 
 if __name__ == '__main__':
-    main = Main()
-    main.configure_traits()
+    mainWin = MainWindow()
+    mainWin.configure_traits()
 
 
