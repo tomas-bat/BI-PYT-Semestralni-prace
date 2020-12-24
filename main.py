338f9f89bdd2b91a453a2dc13fa8c5cb8028b1ae
Tomáš Batěk <batekto2@fit.cvut.cz>
2020-12-25 00:02:55 +0100

    Check if generator exists

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
 from src.Application import Application
+from traitsui.message import *
 
 if __name__ == '__main__':
     app = Application()
+    if not os.path.exists('generator'):
+        message('Generator executable not found.\nUse get_generator.sh', title='Error')
+        exit(1)
     app.configure_traits()
