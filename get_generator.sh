9a07410998fc0bee82ad627613fd64b51c831d7a
Tomáš Batěk <batekto2@fit.cvut.cz>
2020-12-24 23:28:17 +0100

    Finish get_generator.sh

---
+++ get_generator.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+rm -rf .generator
+
+echo "=========== Cloning repository... ==========="
+
+git clone https://github.com/tomas-bat/ASCII-art-generator .generator
+RES=$?
+
+if [ $RES -ne 0 ]
+then
+	echo "Error: Failed cloning repository."
+	rm -rf .generator
+	exit 1
+fi
+
+echo "=========== Repository cloned into .generator ==========="
+echo "=========== Compiling C++... ==========="
+
+cd .generator
+make compile
+RES=$?
+
+if [ $RES -ne 0 ]
+then
+	echo "Error: Failed compiling C++."
+	cd ..
+	rm -rf .generator
+	exit 1
+fi
+
+echo "=========== C++ compiled ==========="
+echo "=========== Moving generator executable... ==========="
+
+cp generator ../generator
+RES=$?
+
+if [ $RES -ne 0 ]
+then
+	echo "Error: Failed moving generator executable."
+	cd ..
+	rm -rf .generator
+	exit 1
+fi
+
+cd ..
+rm -rf .generator
+
+echo "=========== Finished. ==========="
